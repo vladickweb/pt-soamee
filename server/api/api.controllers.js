cb49092f1d0de0b93ede4c1ba1b88a2d2b7ec2e7
Vladyslav <vladickbaraza@gmail.com>
2021-11-10 11:41:16 +0100

    done

--- server/api/api.controllers.js
+++ server/api/api.controllers.js
@@ -3,7 +3,7 @@ const Author = require('./models/Author.model')
 
 exports.getBooks = (req, res, next) => {
 	Book.find()
-    .populate('author')
+		.populate('author')
 		.then(books => res.status(200).json({ status: 200, books }))
 		.catch(err => res.status(500).json({ status: 500, errMsg: err }))
 }
@@ -12,6 +12,7 @@ exports.getOneBook = (req, res) => {
 	const { id } = req.params
 
 	Book.findById(id)
+		.populate('author')
 		.then(book => res.status(200).json({ Code: 200, book }))
 		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
 }
@@ -41,25 +42,45 @@ exports.createBook = (req, res) => {
 exports.createAuthor = (req, res) => {
 	const { first_name, last_name } = req.body
 
-  Author.create({ first_name, last_name })
-    .then(author => res.status(200).json({ Code: 200, author }))
-    .catch(err => res.status(500).json({ Code: 500, errMsg: err }))
+	Author.create({ first_name, last_name })
+		.then(author => res.status(200).json({ Code: 200, author }))
+		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
 }
 
 exports.updateBook = (req, res) => {
 	const { id } = req.params
 	const { name, isbn, author } = req.body
+	const authorId = author._id
 
-  Book.findByIdAndUpdate(id, { name, isbn, author }, { new: true })
-    .then(book => res.status(200).json({ Code: 200, book }))
-    .catch(err => res.status(500).json({ Code: 500, errMsg: err }))
+	Book.findByIdAndUpdate(id, { name, isbn, authorId }, { new: true })
+		.then(book => res.status(200).json({ Code: 200, book }))
+		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
 }
 
 exports.updateAuthor = (req, res) => {
 	const { id } = req.params
 	const { first_name, last_name } = req.body
 
-  Author.findByIdAndUpdate(id, { first_name, last_name }, { new: true })
-    .then(author => res.status(200).json({ Code: 200, author }))
-    .catch(err => res.status(500).json({ Code: 500, errMsg: err }))
+	Author.findByIdAndUpdate(id, { first_name, last_name }, { new: true })
+		.then(author => res.status(200).json({ Code: 200, author }))
+		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
+}
+
+exports.deleteBook = (req, res) => {
+	const { id } = req.params
+
+	Book.findByIdAndDelete(id)
+		.then(() => res.status(200).json({ Code: 200 }))
+		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
+}
+
+exports.deleteAuthor = (req, res) => {
+	const { id } = req.params
+
+	const removeBooks = Book.deleteMany({ author: id })
+	const removeAuthor = Author.findByIdAndDelete(id)
+
+	Promise.all([ removeBooks, removeAuthor ])
+		.then(() => res.status(200).json({ Code: 200 }))
+		.catch(err => res.status(500).json({ Code: 500, errMsg: err }))
 }
--- server/api/api.routes.js
+++ server/api/api.routes.js
@@ -6,9 +6,11 @@ router.get('/books', ApiControllers.getBooks)
 router.get('/book/:id', ApiControllers.getOneBook)
 router.post('/book', ApiControllers.createBook)
 router.put('/book/:id', ApiControllers.updateBook)
+router.delete('/book/:id', ApiControllers.deleteBook)
 router.get('/authors', ApiControllers.getAuthors)
 router.get('/author/:id', ApiControllers.getOneAuthor)
 router.post('/author', ApiControllers.createAuthor)
 router.put('/author/:id', ApiControllers.updateAuthor)
+router.delete('/author/:id', ApiControllers.deleteAuthor)
 
 module.exports = router
